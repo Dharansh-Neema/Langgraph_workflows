7af6f0c34d0b8feb36b99c99cb403655791c92f9
Dharansh-Neema <dharanshneema@gmail.com>
2025-04-10 13:43:34 +0530

    In RAG completed till storing of data in pinecone

--- RAG_OR_LLM/rag.py
+++ RAG_OR_LLM/rag.py
@@ -2,6 +2,7 @@ from langchain_community.document_loaders import TextLoader
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import logging
 import os 
 #env variables
@@ -12,12 +13,29 @@ logger = logging.getLogger(name="RAG")
 loader = TextLoader('./data/indian_gdp.txt')
 data = loader.load()
 logger.debug("Extracted the info")
-print(data[0].page_content)
-
-
+# print(data[0].page_content)
+cont = data[0].page_content
 #Embedding model
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 #Vector db
 pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
-index = pc.Index("Indian_GDP")
+# pc.create_index(name="indian-gdp",
+#                 dimension=768,
+#                 metric="cosine",
+#                 spec={
+#                     "serverless": {
+#                             "region": "us-east-1",
+#                             "cloud": "aws"}})
+index = pc.Index("indian-gdp")
 vector_store = PineconeVectorStore(embedding=embeddings,index=index)
+
+#Text-Splitter 
+text_spliter = RecursiveCharacterTextSplitter(chunk_size=100,chunk_overlap=20)
+splits = text_spliter.split_documents(data)
+# print(splits)
+logger.debug("Implemented text splitter successfully")
+
+# Vector store 
+doc_ids = vector_store.add_documents(splits)
+# print(doc_ids[:5])
+logger.debug("Successfully stored the result")

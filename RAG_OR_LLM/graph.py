e9e64db6da77961043dc79f5a2bfb5ec51908b00
Dharansh-Neema <dharanshneema@gmail.com>
2025-04-11 00:45:13 +0530

    Completed graph workflow

--- RAG_OR_LLM/graph.py
+++ RAG_OR_LLM/graph.py
@@ -1,18 +1,18 @@
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph,START,END
 from retrival import generate
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os 
-from pydantic_structure import Router
+from pydantic_structure import Router,AgentState
 from typing import Literal
-
-def router_agent(AgentState)->Literal["LLM","RAG"]:
+from IPython.display import Image, display
+def router_agent(agentState):
     """This is router agent which will router between LLM or RAG"""
     try:
         llm = ChatGoogleGenerativeAI(
                     model="gemini-2.0-flash-001",
                     api_key=os.getenv('GOOGLE_API_KEY'))
         llm_output = llm.with_structured_output(Router)
-        query = AgentState['query']
+        query = agentState['query']
         
         prompt = """You have been given with this query:{query}, now analyse the query and decide whether to call
         RAG or LLM.
@@ -21,49 +21,78 @@ def router_agent(AgentState)->Literal["LLM","RAG"]:
         
         output = llm_output.invoke(formated_prompt)
         print(output)
-        return output
+        agentState['router'] = output.classification
+        # return output.classification
+        return agentState
     except Exception as e:
         print("Unexpected error occurred on router_agent",e)
         raise e
 
-def llm_call(AgentState):
+def llm_call(agentState):
     """This is LLM which will reply for any other query."""
     try:
-        query = AgentState['query']
+        query = agentState['query']
         llm = ChatGoogleGenerativeAI(
                         model="gemini-2.0-flash-001",
                         api_key=os.getenv('GOOGLE_API_KEY'))
         output = llm.invoke(query)
-        print(output.content)
-        answer = AgentState['answer']
+        answer = agentState['answer']
         answer.append(output.content)
-        AgentState['answer'] = answer
-        print(AgentState)
-        return AgentState
+        agentState['answer'] = answer
+        print(agentState)
+        return agentState
     except Exception as e:
         print("Some unexpected error occurred at LLM CALL",e)
         raise e
 
-def rag_call(AgentState):
+def rag_call(agentState):
     """ This is RAG CALL """
     try:
-        query = AgentState['query']
+        query = agentState['query']
         output = generate(question=query)
-        answer = AgentState['answer']
+        answer = agentState['answer']
         answer.append(output)
-        AgentState['answer'] = answer
-        print(AgentState)
-        return AgentState
+        agentState['answer'] = answer
+        print(agentState)
+        return agentState
     
     except Exception as e:
         print("Some unexpected error occurred at RAG CALL",e)
         raise e
+    
+def invoke_graph(agentState):
+    """This function will invoke the graph"""
+    graph  = StateGraph(AgentState)
+    graph = graph.add_node("router_agent",router_agent)
+    graph = graph.add_node("LLM",llm_call)
+    graph = graph.add_node("RAG",rag_call)
+    graph = graph.set_entry_point("router_agent")
+    graph = graph.add_conditional_edges("router_agent",
+        lambda state : state['router'],
+        {
+            "RAG":"RAG",
+            "LLM":"LLM"
+        }
+    )
+    graph = graph.add_edge("RAG",END)
+    graph = graph.add_edge("LLM",END)
+    graph = graph.compile()
+    # display(
+    # Image(
+    #     graph.get_graph().draw_mermaid_png()
+    # ))
+    # with open("graph_architecture.png", "wb") as f:
+    #     f.write(graph.get_graph().draw_mermaid_png())
+    graph.invoke(agentState)
+
+
 
 if __name__ == "__main__":
-    AgentState = {}
+    agentState = {}
     q = "What will be the GDP of India in 2025."
-    q_l = "What is Langchain?"
-    AgentState.update(query=q)
-    AgentState.update(answer=[])
+    q_l = "What is Langchain in 200 words?"
+    agentState.update(query=q_l)
+    agentState.update(answer=[])
+
     # print(AgentState)
-    rag_call(AgentState)
+    invoke_graph(agentState)

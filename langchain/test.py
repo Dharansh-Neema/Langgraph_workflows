1556e6b455687ad5fc11a93d5a8e35e39bba58fb
Dharansh-Neema <dharanshneema@gmail.com>
2026-06-13 20:33:56 +0530

    Langchain init_model example

--- langchain/test.py
+++ langchain/test.py
@@ -1,12 +1,30 @@
 from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
+from typing import List
 load_dotenv()
 
+model = init_chat_model(model='google_genai:gemini-3.1-flash-lite')
 def testing(prompt):
-    model = init_chat_model(model='google_genai:gemini-3.1-flash-lite')
+   
     response = model.invoke(prompt)
     # print(response)
     return response.content[0]['text']
 
+def streaming(prompt):
+    
+    response = model.stream(prompt)
+    for chunk in response:
+        print(chunk.text,end="",flush=True)
+
+
+def batching(prompts: List):
+    print(model.batch(prompts))
+
 if __name__ == '__main__':
-    print(testing("Hi what's capital of USA"))
+    # print(testing("Hi what's capital of USA"))
+    # streaming("Describe about banswara a city in rajasthan in 1000 words ")
+    prompts = [
+        "What should a Associate software enginner do to get hired as AI enginner in TOP fintech or banks",
+        "Give some good projects ideas for AI-Enginner Industry standard types"
+    ]
+    batching(prompts)
